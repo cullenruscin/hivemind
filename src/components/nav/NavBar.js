b5a8f14f142c4a0a8e709c682fc9f3dd97b437bf
Cullen Ruscin <cullen.ruscin@icloud.com>
2023-04-24 22:13:08 -0500

    Add app view components

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -8,11 +8,11 @@ export const NavBar = () => {
         <nav className="navbar is-light">
             <div className="navbar-menu">
                 <div className="navbar-start">
-                    <Link className="navbar-item">Home</Link>
+                    <Link className="navbar-item" to="/home">Home</Link>
                 </div>
                 <div className="navbar-end">
                     <Link className="navbar-item">+</Link>
-                    <Link className="navbar-item">Profile</Link>
+                    <Link className="navbar-item" to="/profile">Profile</Link>
                     <Link className="navbar-item" to="" onClick={() => {
                         localStorage.removeItem("hivemind_user")
                         navigate("/", {replace: true})
--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,7 +1,18 @@
 import { Outlet, Route, Routes } from "react-router-dom";
+import { PostList } from "../posts/PostList";
+import { UserProfile } from "../profile/UserProfile";
 
 export const ApplicationViews = () => {
     return <>
-        <p>Application views is working!</p>
+        <Routes>
+            <Route path="/" element={
+                <>
+                    <Outlet/>
+                </>
+            }>
+                <Route path="home" element={ <PostList/> }/>
+                <Route path="profile" element={ <UserProfile/> }/>
+            </Route>
+        </Routes>
     </>
 }
