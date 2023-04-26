44af94f577956deebedbbcb45cc984c7fa10cbaf
Cullen Ruscin <cullen.ruscin@icloud.com>
2023-04-26 13:36:12 -0500

    Add delete button

--- src/components/profile/ProfilePosts.js
+++ src/components/profile/ProfilePosts.js
@@ -3,15 +3,15 @@ import { PostList } from "../posts/PostList";
 
 export const ProfilePosts = ({ setActiveTab }) => {
 
+    const localUser = localStorage.getItem("hivemind_user");
+    const userObject = JSON.parse(localUser);
+
     useEffect(
         () => {
             setActiveTab(1)
         },
         []
     );
-
-    const localUser = localStorage.getItem("hivemind_user");
-    const userObject = JSON.parse(localUser);
 
     return <>
         <PostList filter={`userId=${userObject.id}`}/>
